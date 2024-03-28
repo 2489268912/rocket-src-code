8fcc34eb1890df5cc146c578c233f1cc2a9b4767
24892612 <2489268912@qq.com>
2024-03-28 23:55:42 +0800

    first commit

---
+++ snapshoot/instcounter.scala
@@ -0,0 +1,35 @@
+package freechips.rocketchip.snapshoot
+
+import chisel3._
+import chisel3.util._
+
+
+class Instcounter extends Module{
+   val io = IO(new Bundle{
+      val wb_valid    =    Input(Bool())
+      val copy_valid  =    Output(Bool())
+      val copy_done   =    Input(Bool())
+})
+   val instnumber     =    RegInit(0.U(64.W))
+   val fpcsrreg       =    RegInit(0.U(65.W))
+   
+//copy request
+when(io.wb_valid){
+   instnumber := instnumber + 1.U
+}
+.otherwise{
+   instnumber := instnumber
+}
+when(instnumber === 100.U){
+   io.copy_valid := true.B
+}
+.otherwise{
+   io.copy_valid := false.B
+}
+
+dontTouch(instnumber)
+
+when(io.copy_done){
+   instnumber := 0.U
+}
+}
